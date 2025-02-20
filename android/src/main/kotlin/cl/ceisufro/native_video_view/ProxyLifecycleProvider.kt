dfcaa5fc078e1ea64912f9ffd9282a1483797dc1
SergeyPetrovi4 <sergey.krasiuk@gmail.com>
2025-02-20 16:35:43 +0200

    Update ProxyLifecycleProvider.kt

--- android/src/main/kotlin/cl/ceisufro/native_video_view/ProxyLifecycleProvider.kt
+++ android/src/main/kotlin/cl/ceisufro/native_video_view/ProxyLifecycleProvider.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.LifecycleRegistry
 
 class ProxyLifecycleProvider(activity: Activity) : Application.ActivityLifecycleCallbacks,
     LifecycleOwner, LifecycleProvider {
-    override val lifecycle: LifecycleRegistry = LifecycleRegistry(this)
+    override public val lifecycle: LifecycleRegistry = LifecycleRegistry(this)
     private val registrarActivityHashCode: Int = activity.hashCode()
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
